e717cef99a776af6b133148053cf830cc48890f4
Rai Lee <ralee@e1z3r5p24.42.us.org>
2017-11-28 18:01:30 -0800

    more work

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/11/27 15:09:51 by ralee             #+#    #+#              #
-#    Updated: 2017/11/28 10:33:59 by ralee            ###   ########.fr        #
+#    Updated: 2017/11/28 17:53:33 by ralee            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,8 +15,9 @@ SRC = ./src/
 CHECKER = -Wall -Wextra -Werror
 INCLUDE_PATH = ./include/
 OPTION = -c -I $(INCLUDE_PATH)
-SOURCE = $(SRC)ft_strlen.c $(SRC)ft_strdup.c $(SRC)ft_strcpy.c $(SRC)ft_strncpy.c
-O_FILES = ft_strlen.o ft_strdup.o ft_strcpy.o ft_strncpy.o
+SOURCE = $(SRC)ft_strlen.c $(SRC)ft_strdup.c $(SRC)ft_strcpy.c $(SRC)ft_strncpy.c \
+$(SRC)ft_strcmp.c $(SRC)ft_atoi.c $(SRC)ft_isalpha.c $(SRC)ft_isdigit.c
+O_FILES = ft_strlen.o ft_strdup.o ft_strcpy.o ft_strncpy.o ft_strcmp.o ft_atoi.o ft_isalpha.o ft_isdigit.o
 
 all: $(NAME)
 
@@ -26,7 +27,7 @@ $(NAME):
 
 
 clean:
-	/bin/rm -f $(O-FILES)
+	/bin/rm -r $(O_FILES)
 
 fclean: clean
 	/bin/rm -f $(NAME)
---
+++ src/ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/28 14:19:23 by ralee             #+#    #+#             */
+/*   Updated: 2017/11/28 17:50:48 by ralee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_atoi(char *str)
+{
+	int		start;
+	long long	number;
+	int		positive;
+	int		placeholder;
+
+	start = 0;
+	number = 0;
+	positive = 1;
+	while (str[start] == '\t' || str[start] == '\n' || str[start] == '\v' ||
+	str[start] == '\f' || str[start] == '\r' || str[start] == ' ')
+		start++;
+	if (str[start] == '-' || str[start] == '+')
+	{
+		if (str[start] == '-')
+			positive = -1;
+		start++;
+	}
+	while (str[start] >= '0' && str[start] <= '9')
+	{
+		number = number * 10;
+		placeholder = str[start] - '0';
+		number = number + placeholder;
+		start++;
+	}
+	return ((number * positive));
+}
