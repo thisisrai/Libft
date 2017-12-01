5f2cf50f104622b276e5524ecee698c1a59b5cb2
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-12-01 13:47:42 -0800

    add strcat

--- src/ft_strcat.c
+++ src/ft_strcat.c
@@ -1,22 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/27 14:46:51 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/01 12:25:01 by ralee            ###   ########.fr       */
+/*   Created: 2017/12/01 10:38:31 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/01 13:46:47 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/libft.h"
 
-int		ft_strlen(char *str);
-char	*ft_strdup(char *src);
-int		ft_isdigit(int c);
-int		ft_isalpha(int c);
-char	*ft_strcpy(char *dest, char *src);
+char	*ft_strcat(char *restrict s1, char *restrict s2)
+{
+	char	*start;
+	char	*cpy;
 
-#endif
+	start = s1;
+	cpy = (char*)malloc(sizeof(char) * (ft_strlen(s2) + 1));
+	ft_strcpy(cpy, s2);
+	if (*s2 == '\0')
+		return (s1);
+	while (*s1 != '\0')
+		s1++;
+	while (*cpy != '\0')
+	{
+		s1[0] = cpy[0];
+		s1++;
+		cpy++;
+	}
+	s1[0] = '\0';
+	return (start);
+}
