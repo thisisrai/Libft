dbb9f2ed3948ad76641fcbd493e2a3ff5ff6f72e
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-11-30 17:09:31 -0800

    add strrchr

---
+++ src/ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/30 16:24:42 by ralee             #+#    #+#             */
+/*   Updated: 2017/11/30 17:08:18 by ralee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*holder;
+
+	holder = 0;
+	if (c == 0)
+	{
+		return ((char*)(s + (ft_strlen((char*)s))));
+	}
+	while (*s && *s != '\0')
+	{
+		if (*s == c)
+			holder = (char*)s;
+		s++;
+	}
+	return (holder);
+}
