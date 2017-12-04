155bf09e602d19f1ac67961193e1dc1de5f19001
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-12-04 10:57:04 -0800

    memchr

---
+++ src/ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/04 10:39:37 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/04 10:56:32 by ralee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memchr(void *s, int c, size_t n)
+{
+	size_t			index;
+	unsigned char	*s2;
+	unsigned char	c2;
+
+	s2 = (unsigned char*)s;
+	c2 = (unsigned char)c;
+	index = 0;
+	while (index < n)
+	{
+		if (*s2 == c2)
+			return (s);
+		s++;
+		s2++;
+		index++;
+	}
+	return (0);
+}
