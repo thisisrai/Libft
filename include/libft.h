4ceda44ef55e319ce464f0746600d5520526c61b
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-12-04 17:08:03 -0800

    libft.h update

--- include/libft.h
+++ include/libft.h
@@ -6,13 +6,16 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 14:46:51 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/01 12:25:01 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/04 17:06:31 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <string.h>
 
+int		ft_atoi(char *str);
+void	ft_bzero(void *s, size_t n);
 int		ft_strlen(char *str);
 char	*ft_strdup(char *src);
 int		ft_isdigit(int c);
