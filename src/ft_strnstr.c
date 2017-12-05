6edac55a4903e12b915c88c8181274e9b4fc08d5
Rai Lee <ralee@e1z3r6p19.42.us.org>
2017-12-05 14:58:07 -0800

    strnstr

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -1,48 +1,61 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/29 11:20:23 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/05 12:38:16 by ralee            ###   ########.fr       */
+/*   Created: 2017/12/05 12:38:33 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/05 14:57:30 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
+#include <string.h>
 
-void	looper(int *counter, char **placeholder, char **onebyone)
+int		firstif(int *counter, char **placeholder, char **little, char **big)
+{
+	if (*counter == 0)
+		*big = *placeholder;
+	(*counter)++;
+	if (*counter == ft_strlen(*little))
+		return (1);
+	else
+		return (0);
+}
+
+void	looper2(int *counter, char **placeholder, char **onebyone)
 {
 	*counter = 0;
 	*placeholder = *onebyone;
 	(*onebyone)++;
 }
 
-char	*ft_strstr(char *big, char *little)
+char	*ft_strnstr(char *big, char *little, size_t len)
 {
 	char	*placeholder;
 	char	*onebyone;
 	int		counter;
+	size_t	lessorequallen;
 
+	lessorequallen = 0;
 	counter = 0;
 	placeholder = big;
 	onebyone = big;
 	if (!*little)
 		return (big);
-	while (little[counter] != '\0' && *placeholder != '\0')
+	while (little[counter] != '\0' &&
+	*placeholder != '\0' && lessorequallen < len)
 	{
 		if (little[counter] == *placeholder)
 		{
-			if (counter == 0)
-				big = placeholder;
-			counter++;
-			if (counter == ft_strlen(little))
+			if (firstif(&counter, &placeholder, &little, &big) == 1)
 				return (big);
 		}
 		else
-			looper(&counter, &placeholder, &onebyone);
+			looper2(&counter, &placeholder, &onebyone);
 		placeholder++;
+		lessorequallen++;
 	}
 	return (0);
 }
