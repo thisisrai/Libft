3bf0932864f30c95eb75804a9d9c1e4b9b72fad1
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-12-06 15:27:41 -0800

    strnew

---
+++ src/ft_strnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 14:09:36 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/06 15:26:53 by ralee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+	size_t	counter;
+
+	counter = 0;
+	str = malloc(size + 1);
+	if (str != 0)
+	{
+		while (counter < size)
+		{
+			str[counter] = '\0';
+			counter++;
+		}
+		str[counter] = '\0';
+		return (str);
+	}
+	else
+		return (0);
+}
