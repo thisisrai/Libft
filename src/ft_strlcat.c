3f1fed3ad3bbcebbaaf56e61e5e6e93e7b3c1ce3
Rai Lee <ralee@e1z3r6p19.42.us.org>
2017-12-05 18:09:29 -0800

    strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,13 +6,31 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 14:59:20 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/05 16:23:56 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/05 18:09:13 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <string.h>
+#include <string.h>
+#include "../include/libft.h"
 
-// size_t	strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
-// {
-	
-// }
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
+{
+	size_t	counter;
+	size_t	end;
+
+	counter = 0;
+	end = 0;
+	if (dstsize == 0)
+		return (counter);
+	while (dst[counter] && counter < dstsize)
+		counter++;
+	end = counter;
+	while (src[counter - end] && counter < dstsize - 1)
+	{
+		dst[counter] = src[counter - end];
+		counter++;
+	}
+	if (counter < dstsize)
+		dst[counter] = '\0';
+	return (end + ft_strlen(src));
+}
