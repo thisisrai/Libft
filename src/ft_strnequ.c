6a8b90c333413d018e3d9a9d7b9582a28dfe7082
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-12-07 14:25:10 -0800

    strnequ

---
+++ src/ft_strnequ.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/07 14:21:24 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/07 14:24:43 by ralee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+int		ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	size_t	count;
+
+	count = 0;
+	if (s1 && s2)
+	{
+		while (s1[0] == s2[0] && s1[0] != '\0' && s2[0] != '\0' && count < n)
+		{
+			s1++;
+			s2++;
+			count++;
+		}
+		return (unsigned char)*s1 - (unsigned char)*s2 ? 0 : 1;
+	}
+	return (0);
+}
