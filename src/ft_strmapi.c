0f17d3854a55cf5cac38836226667228256132cc
Rai Lee <ralee@e1z2r9p1.42.us.org>
2017-12-07 14:03:44 -0800

    strmapi

---
+++ src/ft_strmapi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/07 13:58:53 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/07 14:01:40 by ralee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*returnstr;
+	char	*copy;
+	int		index;
+
+	index = 0;
+	copy = (char*)s;
+	returnstr = 0;
+	if (s && f)
+	{
+		returnstr = (char*)malloc(ft_strlen((char*)s) + 1);
+		if (returnstr)
+		{
+			ft_strcpy(returnstr, copy);
+			while (returnstr[index] != '\0')
+			{
+				returnstr[index] = f(index, returnstr[index]);
+				index++;
+			}
+		}
+	}
+	return (returnstr);
+}
