3807ab399f1f8ca66158e333b098574d249bfe16
Rai Lee <thisisrailee@gmail.com>
2017-12-14 16:46:06 -0800

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 14:46:51 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/14 16:26:47 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/14 16:45:44 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,10 +54,19 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
 int		ft_strlen(char *str);
 char	*ft_strmap(char const *s, char (*f)(char));
-
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strncat(char *restrict s1, char *restrict s2, size_t n);
+int		ft_strncmp(char *s1, char *s2, int n);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strnew(size_t size);
 char	*ft_strnstr(char *big, char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
-
+char	**ft_strsplit(const char *str, char c);
+char	*ft_strstr(char *big, char *little);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
