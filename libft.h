201ae913c26610a052cda93bd96a78a14b10e1f9
Rai Lee <thisisrailee@gmail.com>
2017-12-17 12:48:17 -0800

    adding all 6 extra functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 14:46:51 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/17 12:12:42 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/17 12:44:39 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,5 +77,11 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list * ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem));
 
 #endif
