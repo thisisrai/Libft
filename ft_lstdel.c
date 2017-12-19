9714bbd068eb9ea49c074a5612b2bcfd65bcc809
Rai Lee <ralee@e1z3r8p18.42.us.org>
2017-12-19 10:28:49 -0800

    lstdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -1,19 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/19 09:47:04 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/19 09:56:02 by ralee            ###   ########.fr       */
+/*   Created: 2017/12/19 09:55:52 by ralee             #+#    #+#             */
+/*   Updated: 2017/12/19 10:28:32 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd(t_list **alst, t_list *new)
+void	ft_lstdel(t_list **alst, void (*del)(void*, size_t))
 {
-	(*new).next = *alst;
-	*alst = new;
+	t_list	*holder;
+	t_list	*next;
+
+	if (!alst || !*alst)
+		return ;
+	if (del != NULL)
+	{
+		holder = *alst;
+		while (holder)
+		{
+			next = holder->next;
+			del(holder->content, holder->content_size);
+			free(holder);
+			holder = next;
+		}
+	}
+	*alst = NULL;
 }
