4200e9ceb2cecc843210b899813b7c580d1ac53b
Rai Lee <ralee@e1z3r8p18.42.us.org>
2017-12-19 15:05:55 -0800

    more fixes -_-

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 14:21:24 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/18 11:32:35 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/19 14:51:47 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t	count;
 
 	count = 0;
+	if (!n)
+		return (1);
 	if (s1 && s2)
 	{
 		while (s1[0] == s2[0] && s1[0] != '\0'
