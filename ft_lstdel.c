17dd2426f2ec9a21cb301eb3e516663f573d5566
Rai Lee <thisisrailee@gmail.com>
2017-12-20 16:29:32 -0800

    lsdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 09:55:52 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/20 16:07:10 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/20 16:29:04 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@ void	ft_lstdel(t_list **alst, void (*del)(void*, size_t))
 
 	if (!alst || !*alst)
 		return ;
-	if (del != NULL)
-	{
-		holder = *alst;
-		while (holder)
-		{
-			next = holder->next;
-			del(holder->content, holder->content_size);
-			free(holder);
-			holder = next;
-		}
-	}
-	*alst = NULL;
+	// if (del != NULL)
+	// {
+	// 	holder = *alst;
+	// 	while (holder)
+	// 	{
+	// 		next = holder->next;
+	// 		del(holder->content, holder->content_size);
+	// 		free(holder);
+	// 		holder = next;
+	// 	}
+	// }
+	// *alst = NULL;
+	if ((*alst)->next)
+		ft_lstdel(&(*alst)->next, del);
+	ft_lstdelone(alst, del);
 }
