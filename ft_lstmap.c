3a756a8d17f2261fed3313f7a8bb11d69017bdcb
Rai Lee <thisisrailee@gmail.com>
2017-12-20 16:37:14 -0800

    final

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 10:53:01 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/20 16:35:00 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/20 16:36:45 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,6 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	// t_list	*new_list;
-	// t_list	*next;
-	// t_list	*holder;
-
-	// holder = f(lst);
-	// new_list = NULL;
-	// if (lst && f)
-	// {
-	// 	if (!(new_list = ft_lstnew(holder->content, holder->content_size)))
-	// 		return (NULL);
-	// 	holder = new_list;
-	// 	lst = lst->next;
-	// 	while (lst != NULL)
-	// 	{
-	// 		next = f(lst);
-	// 		if (!(holder->next = ft_lstnew(next->content, next->content_size)))
-	// 			return (NULL);
-	// 		holder = holder->next;
-	// 		lst = lst->next;
-	// 	}
-	// }
-	// return (new_list);
-
 	t_list *ret;
 
 	if (!lst || !f)
