1f2d08fab1e654f05f358df10e9a30e068d60586
Rai Lee <thisisrailee@gmail.com>
2017-12-20 16:35:17 -0800

    lstmap fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 10:53:01 by ralee             #+#    #+#             */
-/*   Updated: 2017/12/20 15:46:34 by ralee            ###   ########.fr       */
+/*   Updated: 2017/12/20 16:35:00 by ralee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,39 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*new_list;
-	t_list	*next;
-	t_list	*holder;
+	// t_list	*new_list;
+	// t_list	*next;
+	// t_list	*holder;
 
-	holder = f(lst);
-	new_list = NULL;
-	if (lst && f)
+	// holder = f(lst);
+	// new_list = NULL;
+	// if (lst && f)
+	// {
+	// 	if (!(new_list = ft_lstnew(holder->content, holder->content_size)))
+	// 		return (NULL);
+	// 	holder = new_list;
+	// 	lst = lst->next;
+	// 	while (lst != NULL)
+	// 	{
+	// 		next = f(lst);
+	// 		if (!(holder->next = ft_lstnew(next->content, next->content_size)))
+	// 			return (NULL);
+	// 		holder = holder->next;
+	// 		lst = lst->next;
+	// 	}
+	// }
+	// return (new_list);
+
+	t_list *ret;
+
+	if (!lst || !f)
+		return (NULL);
+	if (lst->next)
 	{
-		if (!(new_list = ft_lstnew(holder->content, holder->content_size)))
-			return (NULL);
-		holder = new_list;
-		lst = lst->next;
-		while (lst != NULL)
-		{
-			next = f(lst);
-			if (!(holder->next = ft_lstnew(next->content, next->content_size)))
-				return (NULL);
-			holder = holder->next;
-			lst = lst->next;
-		}
+		ret = ft_lstmap(lst->next, f);
+		ft_lstadd(&ret, f(lst));
 	}
-	return (new_list);
+	else
+		ret = f(lst);
+	return (ret);
 }
